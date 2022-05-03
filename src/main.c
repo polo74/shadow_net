566e648300c3dcd63a801620bdeb4a93df839641
Paul Rannou <rannoupaul74@gmail.com>
2022-05-03 03:53:05 +0200

    change Z to first layer

--- src/main.c
+++ src/main.c
@@ -11,9 +11,9 @@
  * matrix: 3 dim
  */
 
-typedef float data_t;
+typedef double data_t;
 
-// A voir...
+// not implemented yet
 typedef struct {
     data_t*** array;
     int x, y, z;
@@ -25,8 +25,8 @@ void sub_mask(data_t*** matrix, data_t*** random, int x, int y, int z);
 data_t*** init_matrix(int x, int y, int z);
 data_t** init_table(int x, int y);
 data_t* init_array(int x);
-void free_matrix(data_t*** matrix, int x, int y);
-void free_table(data_t** table, int x);
+void free_matrix(data_t*** matrix, int y, int z);
+void free_table(data_t** table, int y);
 void free_array(data_t* array);
 
 void print_matrix(data_t*** matrix, int x, int y, int z);
@@ -38,39 +38,42 @@ void sub_matrix(data_t*** matrix, data_t*** sub, int x, int y, int z);
 void rand_matrix(data_t*** rand, int x, int y, int z);
 void relu_matrix(data_t*** matrix, int x, int y, int z);
 void flatten_matrix(data_t*** matrix, data_t* array, int x, int y, int z, bool forward);
-// TODO: utiliser la matrice (pour faire comme dans les autres fonctions)
 void average_pool(data_t*** matrix, data_t*** average, int table_x, int table_y, int table_z, int average_x, int average_y, int average_z, int win_x, int win_y);
 void softmax(data_t* array, int x);
 
 int main(void){
 
-    /*
-    int table_x = 2;
-    int table_y = 2;
-    int z = 2;
 
-    data_t*** data = init_matrix(table_x, table_y, z);
-    for (int i = 0; i < table_x; i++){
+    srand(time(NULL));
+
+    /*
+    int table_x = 3;
+    int table_y = 3;
+    int table_z = 3;
+
+    data_t*** data = init_matrix(table_x, table_y, table_z);
+    for (int k = 0; k < table_z; k++){
         for (int j = 0; j < table_y; j++){
-            for (int k = 0; k < z; k++){
-                data[i][j][k] = i+j+k;
+            for (int i = 0; i < table_x; i++){
+                data[k][j][i] = 0;
             }
         }
     }
 
-    data_t* array = init_array(table_x * table_y * z);
-    flatten_matrix(data, array, table_x, table_y, z, true);
-    print_array(array, table_x * table_y * z);
+    data_t* array = init_array(table_x * table_y * table_z);
+    flatten_matrix(data, array, table_x, table_y, table_z, true);
+    print_array(array, table_x * table_y * table_z);
 
-    print_matrix(data, table_x, table_y, z);
-    data_t*** random = add_mask(data, table_x, table_y, z);
-    print_matrix(data, table_x, table_y, z);
-    sub_mask(data, random, table_x, table_y, z);
-    print_matrix(data, table_x, table_y, z);
+    print_matrix(data, table_x, table_y, table_z);
+    data_t*** random = add_mask(data, table_x, table_y, table_z);
+    print_matrix(data, table_x, table_y, table_z);
+    sub_mask(data, random, table_x, table_y, table_z);
+    print_matrix(data, table_x, table_y, table_z);
 
-    free(data);
+    free_matrix(data, table_x, table_y);
+    free(array);
 
-     */
+    */
 
     /*
 
@@ -84,10 +87,10 @@ int main(void){
     int average_z = table_z;
 
     data_t ***table = init_matrix(table_x, table_y, table_z);
-    for (int k = 0; k < table_z; ++k) {
-        for (int i = 0; i < table_x; ++i) {
-            for (int j = 0; j < table_y; ++j) {
-                table[i][j][k] = 2;
+    for (int k = 0; k < table_z; k++) {
+        for (int i = 0; i < table_x; i++) {
+            for (int j = 0; j < table_y; j++) {
+                table[k][j][i] = 2;
             }
         }
     }
@@ -100,8 +103,8 @@ int main(void){
     printf("\n");
     print_matrix(average, average_x, average_y, average_z);
 
-    free_matrix(table, table_x, table_y);
-    free_matrix(average, average_x, average_y);
+    free_matrix(table, table_y, table_z);
+    free_matrix(average, average_y, average_z);
 
      */
 
@@ -113,14 +116,14 @@ int main(void){
     data[1] = 3;
     data[2] = 2;
     print_array(data, x);
-    printf("\n\n");
+    printf("\n");
     softmax(data, x);
     print_array(data, x);
 
-     */
+    */
 
-    int x = 16;
-    int y = 16;
+    int x = 512;
+    int y = 256;
     int z = 16;
 
     int win_x = 1;
@@ -129,7 +132,7 @@ int main(void){
     int average_x = x / win_x;
     int average_y = y / win_y;
     int average_z = z;
-    
+
     int flat_x = x * y * z;
 
     data_t*** data = init_matrix(x, y, z);
@@ -138,14 +141,14 @@ int main(void){
 
     //conv'
     //GO TEE
-    //linear transfo = anti conv'
+    //linear transformation = anti conv'
     //ReLU+Batchnorm
 
-    for (int i = 0; i < 100; ++i) {
+    for (int i = 0; i < 100; i++) {
         data_t ***mask = add_mask(data, x, y, z);
         //OUT TEE --> conv'
         //GO TEE
-        //Linear transfo = anti conv'
+        //Linear transformation = anti conv'
         sub_mask(data, mask, x, y, z);
         relu_matrix(data, x, y, z);
         // BatchNorm
@@ -172,15 +175,15 @@ data_t*** add_mask(data_t*** matrix, int x, int y, int z){
 
 void sub_mask(data_t*** matrix, data_t*** random, int x, int y, int z){
     sub_matrix(matrix, random, x, y, z);
-    free(random);
+    free_matrix(random, y, z);
 }
 
 data_t*** init_matrix(int x, int y, int z){
-    data_t*** matrix = malloc(sizeof(data_t***) * x * y * z);
-    for (int i = 0; i < x; ++i) {
-        matrix[i] = malloc(sizeof(data_t**) * y * z);
-        for (int j = 0; j <y; ++j) {
-            matrix[i][j] = malloc(sizeof(data_t *) * z);
+    data_t*** matrix = malloc(sizeof(data_t***) * z * y * x);
+    for (int k = 0; k < z; k++) {
+        matrix[k] = malloc(sizeof(data_t**) * y * x);
+        for (int j = 0; j <y; j++) {
+            matrix[k][j] = malloc(sizeof(data_t *) * x);
         }
     }
     return matrix;
@@ -188,8 +191,8 @@ data_t*** init_matrix(int x, int y, int z){
 
 data_t** init_table(int x, int y){
     data_t** table = malloc(sizeof(data_t**) * x * y);
-    for (int i = 0; i < x; ++i){
-        table[i] = malloc(sizeof(data_t*) * y);
+    for (int j = 0; j < y; j++){
+        table[j] = malloc(sizeof(data_t*) * x);
     }
     return table;
 }
@@ -198,19 +201,19 @@ data_t* init_array(int x){
     return malloc(sizeof(data_t) * x);
 }
 
-void free_matrix(data_t*** matrix, int x, int y){
-    for (int i = 0; i < x; ++i) {
-        for (int j = 0; j < y; ++j) {
-            free(matrix[i][j]);
+void free_matrix(data_t*** matrix, int y, int z){
+    for (int k = 0; k < z; k++) {
+        for (int j = 0; j <y; j++) {
+            free(matrix[k][j]);
         }
-        free(matrix[i]);
+        free(matrix[k]);
     }
     free(matrix);
 }
 
-void free_table(data_t** table, int x){
-    for (int i = 0; i < x; ++i) {
-        free(table[i]);
+void free_table(data_t** table, int y){
+    for (int j = 0; j < y; j++) {
+        free(table[j]);
     }
     free(table);
 }
@@ -220,11 +223,11 @@ void free_array(data_t* array){
 }
 
 void print_matrix(data_t*** matrix, int x, int y, int z){
-    printf("\n");
-    for (int i = 0; i < x; ++i) {
+    printf("x y z\tvalue\n");
+    for (int k = 0; k < z; k++) {
         for (int j = 0; j < y; j++) {
-            for (int k = 0; k < z; k++) {
-                printf("%d %d %d\t %f\n", i, j, k, matrix[i][j][k]);
+            for (int i = 0; i < x; i++) {
+                printf("%d %d %d\t %f\n", i, j, k, matrix[k][j][i]);
             }
         }
     }
@@ -232,8 +235,8 @@ void print_matrix(data_t*** matrix, int x, int y, int z){
 
 void print_table(data_t** table, int x, int y){
     printf("\n");
-    for (int i = 0; i < x; ++i) {
-        for (int j = 0; j < y; j++) {
+    for (int j = 0; j < y; j++) {
+        for (int i = 0; i < x; i++) {
             printf("%d %d\t %f\n", i, j, table[i][j]);
         }
     }
@@ -241,50 +244,49 @@ void print_table(data_t** table, int x, int y){
 
 void print_array(data_t* array, int x){
     for (int i = 0; i < x; i++){
-        printf("%f ", array[i]);
+        printf("%f\n", array[i]);
     }
 }
 
 void rand_matrix(data_t*** random, int x, int y, int z){
-    srand(time(NULL));
-    for (int i = 0; i < x; i++){
+    for (int k = 0; k < z; k++){
         for (int j = 0; j < y; j++){
-            for (int k = 0; k < z; k++){
-                random[i][j][k] = rand();
+            for (int i = 0; i < x; i++){
+                random[k][j][i] = rand() % 10;
             }
         }
     }
 }
 
 void add_matrix(data_t*** matrix, data_t*** add, int x, int y, int z){
-    for (int i = 0; i < x; ++i) {
-        for (int j = 0; j < y; ++j) {
-            for (int k = 0; k < z; ++k) {
-                matrix[i][j][k] = matrix[i][j][k] + add[i][j][k];
+    for (int k = 0; k < z; k++) {
+        for (int j = 0; j < y; j++) {
+            for (int i = 0; i < x; i++) {
+                matrix[k][j][i] = matrix[k][j][i] + add[k][j][i];
             }
         }
     }
 }
 
 void sub_matrix(data_t*** matrix, data_t*** sub, int x, int y, int z){
-    for (int i = 0; i < x; ++i) {
-        for (int j = 0; j < y; ++j) {
-            for (int k = 0; k < z; ++k) {
-                matrix[i][j][k] = matrix[i][j][k] - sub[i][j][k];
+    for (int k = 0; k < z; k++) {
+        for (int j = 0; j < y; j++) {
+            for (int i = 0; i < x; i++) {
+                matrix[k][j][i] = matrix[k][j][i] - sub[k][j][i];
             }
         }
     }
 }
 
 void relu_matrix(data_t*** matrix, int x, int y, int z){
-    for (int i = 0; i < x; ++i) {
-        for (int j = 0; j < y; ++j) {
-            for (int k = 0; k < z; ++k) {
-                if (matrix[i][j][k] < 0){
-                    matrix[i][j][k] = 0;
+    for (int k = 0; k < z; k++) {
+        for (int j = 0; j < y; j++) {
+            for (int i = 0; i < x; i++) {
+                if (matrix[k][j][i] < 0){
+                    matrix[k][j][i] = 0;
                 }
-                if (matrix[i][j][k] > 6){
-                    matrix[i][j][k] = 6;
+                if (matrix[k][j][i] > 6){
+                    matrix[k][j][i] = 6;
                 }
             }
         }
@@ -292,14 +294,14 @@ void relu_matrix(data_t*** matrix, int x, int y, int z){
 }
 
 void flatten_matrix(data_t*** matrix, data_t* array, int x, int y, int z, bool forward){
-    for (int i = 0; i < x; ++i) {
-        for (int j = 0; j < y; ++j) {
-            for (int k = 0; k < z; ++k) {
-                int pos = i + x * j + k * x * y;
+    for (int k = 0; k < z; k++) {
+        for (int j = 0; j < y; j++) {
+            for (int i = 0; i < x; i++) {
+                int pos = i + j * y + k * y * x;
                 if (forward)
-                    array[pos] = matrix[i][j][k];
+                    array[pos] = matrix[k][j][i];
                 else
-                    matrix[i][j][k] = array[pos];
+                    matrix[k][j][i] = array[pos];
             }
         }
     }
@@ -308,30 +310,30 @@ void flatten_matrix(data_t*** matrix, data_t* array, int x, int y, int z, bool f
 void average_pool(data_t*** matrix, data_t*** average, int table_x, int table_y, int table_z, int average_x, int average_y, int average_z, int win_x, int win_y) {
     //Width must be divisible by X
 
-    for (int i = 0; i < average_x; ++i) {
-        for (int j = 0; j < average_y; ++j) {
-            for (int k = 0; k < average_z; ++k) {
-                average[i][j][k] = 0;
+    for (int k = 0; k < average_z; k++) {
+        for (int j = 0; j < average_y; j++) {
+            for (int i = 0; i < average_x; i++) {
+                average[k][j][i] = 0;
             }
         }
     }
-
-    for (int k = 0; k < table_z; ++k) {
-        for (int i = 0; i < table_x; i += win_x) {
-            for (int j = 0; j < table_y; j += win_y) {
-                for (int a = 0; a < win_x; ++a) {
-                    for (int b = 0; b < win_y; ++b) {
-                        average[i / win_x][j / win_y][k] += matrix[i + a][j + b][k];
+// Todo
+    for (int k = 0; k < table_z; k++) {
+        for (int j = 0; j < table_y; j += win_y) {
+            for (int i = 0; i < table_x; i += win_x) {
+                for (int b = 0; b < win_y; ++b) {
+                    for (int a = 0; a < win_x; ++a) {
+                        average[k][j / win_y][i / win_x] += matrix[k][j + b][i + a];
                     }
                 }
             }
         }
     }
 
-    for (int k = 0; k < average_z; ++k) {
-        for (int i = 0; i < average_x; ++i) {
-            for (int j = 0; j < average_y; ++j) {
-                average[i][j][k] = average[i][j][k] / (win_x * win_y);
+    for (int k = 0; k < average_z; k++) {
+        for (int j = 0; j < average_y; j++) {
+            for (int i = 0; i < average_x; i++) {
+                average[k][j][i] = average[k][j][i] / (win_x * win_y);
             }
         }
     }
@@ -341,10 +343,27 @@ void softmax(data_t* array, int x){
     data_t sum = 0;
     for (int i = 0; i < x; i++){
         sum += (data_t) exp(array[i]);
-        array[i] = (data_t) exp(array[i]);
+        array[i] = (data_t) exp(array[i]); // Todo: put it on the below 'for' loop
     }
 
-    for (int i = 0; i < x; ++i) {
+    for (int i = 0; i < x; i++) {
         array[i] = array[i] / sum;
+    }
+}
+
+data_t mean(data_t** table, int x, int y) {
+    int n = x * y;
+    int sum = 0;
+    for (int j = 0; j < y; j++) {
+        for (int i = 0; i < x; i++) {
+            sum += table[i][j];
+        }
+    }
+    return (1 / n) * sum;
+}
+
+void batchnorm(data_t*** matrix, int x, int y, int z) {
+    for (int k = 0; k < z; k++) {
+        data_t** table = matrix[z];
     }
 }
